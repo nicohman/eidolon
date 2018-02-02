fe4a4bc3f654f7d7eb83155957fb90783ca01a21
nicohman <nico.hickman@gmail.com>
2018-02-02 08:59:29 -0800

    Add rofi script

---
+++ eidolon.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#A Rofi script. If you don't want to run eidolon menu, you can just run 'rofi -show eidolon -modi eidolon:[path to this script]. This should work with combinations.
+if [ -z "$@" ]
+then
+	ls ~/.config/eidolon/games
+else 
+	eidolon run "$@"
+fi
