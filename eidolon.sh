7ded7faa4694c67a9888c9bb79e6f1ddf4c83e0f
nicohman <nico.hickman@gmail.com>
2018-02-04 15:25:07 -0800

    Fix rofi freezing with script

--- eidolon.sh
+++ eidolon.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #A Rofi script. If you don't want to run eidolon menu, you can just run 'rofi -show eidolon -modi eidolon:[path to this script]. This should work with combinations.
-if [ -z "$@" ]
-then
+if [[ -z "$@"  ]]; then
 	ls ~/.config/eidolon/games
 else 
 	eidolon run "$@"
